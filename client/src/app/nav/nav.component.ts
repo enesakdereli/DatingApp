32d7f6a80528c9a4ea86a24d67492b056d92a07a
enes.akdereli <enes.akdereli@assetgrup.com.tr>
2023-07-01 18:43:59 +0300

    Add routing, auth guard. Update theme with united Bootswatch.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { AccountService } from '../_services/account.service';
 
 export class NavComponent implements OnInit {
   model: any = {}
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +20,7 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: _ => this.router.navigateByUrl('/members'),
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
